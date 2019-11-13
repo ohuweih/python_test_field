a92f288317268eaa56085c02d8de043ae5fd5951
Huweih <huweih@88e9fe53273f.ant.amazon.com>
2019-11-13 15:07:44 -0800

    Making this pycodestyle compliant

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,14 +5,13 @@ import threading
 # Create the parser
 my_parser = argparse.ArgumentParser(description='The Number we will be using')
 
-
 # Add the arguments
-my_parser.add_argument('Number',metavar='number',type=int,help='Number to use')
+my_parser.add_argument('Num', metavar='number', type=int, help='Number to use')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
+input_arg = args.Num
 
-input_arg = args.Number
 
 def FizzBuzz(input_arg):
     if (input_arg % 3 == 0) and (input_arg % 5 == 0):
@@ -21,12 +20,14 @@ def FizzBuzz(input_arg):
         output = input_arg
     print(output)
 
+
 def Buzz(input_arg):
     if (input_arg % 5 == 0):
         output = "Buzz"
     else:
         output = input_arg
     print(output)
+
 
 def Fizz(input_arg):
     if (input_arg % 3 == 0):
@@ -35,11 +36,12 @@ def Fizz(input_arg):
         output = input_arg
     print(output)
 
+
 if __name__ == "__main__":
     # creating thread
-    t1 = threading.Thread(target=FizzBuzz, args=(input_arg,))
-    t2 = threading.Thread(target=Buzz, args=(input_arg,))
-    t3 = threading.Thread(target=Fizz, args=(input_arg,))
+    t1 = threading.Thread(target=FizzBuzz, args=(input_arg, ))
+    t2 = threading.Thread(target=Buzz, args=(input_arg, ))
+    t3 = threading.Thread(target=Fizz, args=(input_arg, ))
 
     # Starting Thread
     t1.start()
